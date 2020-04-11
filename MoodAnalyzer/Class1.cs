ec979e6719756c910528474782f3110cf6c50c9e
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-11 11:39:54 +0530

    Added: MoodAnalyseFactory to Create MoodAnalyse Object

---
+++ MoodAnalyzer/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+
+namespace MoodAnalyzer
+{
+    class MoodAnalyseFactory
+    {
+        /// <summary>
+        /// GetConstructo method to get the instance of class.
+        /// </summary>
+        public static object CreateMoodAnalyse(ConstructorInfo constructor)
+        {
+            object moodAnalyse = Activator.CreateInstance(constructor.DeclaringType);
+            return moodAnalyse;
+        }
+    }
+}
